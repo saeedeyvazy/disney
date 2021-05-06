a9097c05c7e78199694caae5131434dbb3d2acca
saeed eyvazy <saeed.eyvazy@gmail.com>
2021-05-06 11:52:49 +0430

    Add full height to html body

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import Header from './components/Header'
+import Home from './pages/Home'
 
 function App() {
 	return (
-		<div className='App'>
+		<div className='app'>
 			<Header />
+			<Home />
 		</div>
 	)
 }
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Home() {
+	return <Container></Container>
+}
+
+const Container = styled.main`
+	flex: 1;
+	background-color: red;
+`
+
+export default Home
