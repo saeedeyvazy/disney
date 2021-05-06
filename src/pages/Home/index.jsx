3a1caffa4b857c3663de8a29bb5cbf9e12375ddb
saeed eyvazy <saeed.eyvazy@gmail.com>
2021-05-06 12:09:35 +0430

    Add background image to home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import styled from 'styled-components'
+import bg from '../../assets/images/home-background.png'
 
 function Home() {
-	return <Container></Container>
+	return <Container>Hello</Container>
 }
 
 const Container = styled.main`
-	flex: 1;
-	background-color: red;
+	min-height: calc(100vh - 70px);
+	padding: 0 calc(3.5vw + 5px);
+	position: relative;
+	&:before {
+		content: '';
+		background-image: url(${bg});
+		background-size: cover;
+		background-repeat: no-repeat;
+		background-position: center;
+		position: absolute;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		z-index: -1;
+	}
 `
 
 export default Home
