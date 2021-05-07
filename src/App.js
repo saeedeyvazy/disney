555e0d67c7ea32945cde886c6a03f767ba1bf43a
saeed eyvazy <saeed.eyvazy@gmail.com>
2021-05-07 14:02:05 +0430

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from 'react'
 import './App.css'
 import Header from './components/Header'
 import Home from './pages/Home'
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Detail from './pages/Detail'
 function App() {
 	return (
 		<div className='app'>
-			<Header />
-			<Home />
+			<Router>
+				<Header />
+				<Switch>
+					<Route exact path='/' component={Home} />
+					<Route exact path='/detail' component={Detail} />
+				</Switch>
+			</Router>
 		</div>
 	)
 }
