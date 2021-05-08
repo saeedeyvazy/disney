0615670cdd024c6ff3e1e6d256c03a01582e6a0a
saeed eyvazy <saeed.eyvazy@gmail.com>
2021-05-08 11:26:06 +0430

    Add grab movie list from db and add redux :star:

--- src/components/Movies/index.jsx
+++ src/components/Movies/index.jsx
@@ -1,52 +1,56 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react"
+import { useSelector } from "react-redux"
+import styled from "styled-components"
+import { selectMovies } from "../../features/movie/movieSlice"
 
-function Movies({ title, imgList }) {
-	return (
-		<Container>
-			<h4>{title}</h4>
-			<Content>
-				{imgList
-					? imgList.map((item) => (
-							<Wrap>
-								<img src={item} alt='video' />
-							</Wrap>
-					  ))
-					: null}
-			</Content>
-		</Container>
-	)
+function Movies() {
+  const movies = useSelector(selectMovies)
+  console.log(movies)
+  return (
+    <Container>
+      <h4>Recommended For You</h4>
+      <Content>
+        {movies
+          ? movies.map((item) => (
+              <Wrap key={item.id}>
+                <img src={item.cardImg} alt='video' />
+              </Wrap>
+            ))
+          : null}
+      </Content>
+    </Container>
+  )
 }
 
 const Container = styled.div`
-	padding: 20px 0;
+  padding: 20px 0;
 `
 
 const Content = styled.div`
-	display: grid;
-	grid-gap: 25px;
-	grid-template-columns: repeat(4, minmax(0, 1fr));
+  display: grid;
+  grid-gap: 25px;
+  grid-template-columns: repeat(4, minmax(0, 1fr));
 `
 
 const Wrap = styled.div`
-	cursor: pointer;
-	border-radius: 7px;
-	overflow: hidden;
-	transition-duration: 200ms;
-	box-shadow: rgba(0 0 0 / 69%) 0 26px 30px -10px,
-		rgba(0 0 0 /73%) 0 16px 10px -10px;
+  cursor: pointer;
+  border-radius: 7px;
+  overflow: hidden;
+  transition-duration: 200ms;
+  box-shadow: rgba(0 0 0 / 69%) 0 26px 30px -10px,
+    rgba(0 0 0 /73%) 0 16px 10px -10px;
 
-	border: 3px solid rgba(249, 249, 249, 0.1);
-	height: 130px;
-	&:hover {
-		border: 3px solid rgba(249, 249, 249, 0.8);
-		transform: scale(1.05);
-	}
-	img {
-		width: 100%;
-		height: 100%;
-		object-fit: cover;
-	}
+  border: 3px solid rgba(249, 249, 249, 0.1);
+  height: 130px;
+  &:hover {
+    border: 3px solid rgba(249, 249, 249, 0.8);
+    transform: scale(1.05);
+  }
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 `
 
 export default Movies
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,47 +1,50 @@
-import React from 'react'
-import styled from 'styled-components'
-import ImgSlider from '../../components/ImgSlider'
-import Movies from '../../components/Movies'
-import Viewer from '../../components/Viewer'
-import { images } from '../../constants/images'
-
+import React, { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import styled from "styled-components"
+import ImgSlider from "../../components/ImgSlider"
+import Movies from "../../components/Movies"
+import Viewer from "../../components/Viewer"
+import { images } from "../../constants/images"
+import { setMovies } from "../../features/movie/movieSlice"
+import db from "../../firebase"
 function Home() {
-	return (
-		<Container>
-			<ImgSlider />
-			<Viewer />
-			<Movies
-				title='Recommended For You'
-				imgList={[
-					images.slider2,
-					images.slider1,
-					images.slider3,
-					images.slider4,
-				]}
-			/>
-		</Container>
-	)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    db.collection("movies").onSnapshot((snapshot) => {
+      let movies = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+      dispatch(setMovies(movies))
+    })
+  }, [])
+
+  return (
+    <Container>
+      <ImgSlider />
+      <Viewer />
+      <Movies />
+    </Container>
+  )
 }
 
 const Container = styled.main`
-	min-height: calc(100vh - 70px);
-	padding: 0 calc(3.5vw + 5px);
-	position: relative;
-	overflow-x: hidden;
+  min-height: calc(100vh - 70px);
+  padding: 0 calc(3.5vw + 5px);
+  position: relative;
+  overflow-x: hidden;
 
-	&:before {
-		content: '';
-		background-image: url(${images.homeBG});
-		background-size: cover;
-		background-repeat: no-repeat;
-		background-position: center;
-		position: absolute;
-		top: 0;
-		left: 0;
-		right: 0;
-		bottom: 0;
-		z-index: -1;
-	}
+  &:before {
+    content: "";
+    background-image: url(${images.homeBG});
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+  }
 `
 
 export default Home
