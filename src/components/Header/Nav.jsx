f5bea07165177447396c319b6ca7902336d370ee
saeed eyvazy <saeed.eyvazy@gmail.com>
2021-05-08 09:25:10 +0430

    FIX header style :lipsticik:

--- src/components/Header/Nav.jsx
+++ src/components/Header/Nav.jsx
@@ -1,78 +1,79 @@
-import React from 'react'
-import styled from 'styled-components'
-import { images } from '../../constants/images'
+import React from "react"
+import styled from "styled-components"
+import { images } from "../../constants/images"
 
 const Container = styled.nav`
-	display: flex;
-	align-items: center;
-	flex: 1;
-	margin-left: 25px;
-	overflow-x: hidden;
+  display: flex;
+  align-items: center;
+  flex: 1;
+  margin-left: 25px;
+  overflow: hidden;
+  height: 100%;
 `
 
 const MenuItem = styled.a`
-	text-transform: uppercase;
-	cursor: pointer;
-	display: flex;
-	align-items: center;
-	padding: 0 12px;
-	span {
-		font-size: 13px;
-		margin-left: 5px;
-		position: relative;
-		&:after {
-			content: '';
-			height: 5px;
-			background-color: white;
-			position: absolute;
-			left: 0;
-			opacity: 0;
-			right: 0;
-			bottom: -6px;
-			transform: scaleX(0);
-			transform-origin: right left;
-			transition: all 300ms ease-in-out;
-		}
+  text-transform: uppercase;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  padding: 0 12px;
+  span {
+    font-size: 13px;
+    margin-left: 5px;
+    position: relative;
+    &:after {
+      content: "";
+      height: 2px;
+      background-color: white;
+      position: absolute;
+      left: 0;
+      opacity: 0;
+      right: 0;
+      bottom: -6px;
+      transform: scaleX(0);
+      transform-origin: right left;
+      transition: all 300ms ease-in-out;
+    }
 
-		&:hover:after {
-			transform: scaleX(1);
-			opacity: 1;
-		}
-	}
+    &:hover:after {
+      transform: scaleX(1);
+      opacity: 1;
+    }
+  }
 `
 const MenuItemImg = styled.img`
-	height: 20px;
+  height: 20px;
 `
 
 function Nav() {
-	return (
-		<Container>
-			<MenuItem>
-				<MenuItemImg src={images.homeIcon} />
-				<span>home</span>
-			</MenuItem>
-			<MenuItem>
-				<MenuItemImg src={images.searchIcon} />
-				<span>search</span>
-			</MenuItem>
-			<MenuItem>
-				<MenuItemImg src={images.watchIcon} />
-				<span>watchlist</span>
-			</MenuItem>
-			<MenuItem>
-				<MenuItemImg src={images.seriesIcon} />
-				<span>series</span>
-			</MenuItem>
-			<MenuItem>
-				<MenuItemImg src={images.movieIcon} />
-				<span>movies</span>
-			</MenuItem>
-			<MenuItem>
-				<MenuItemImg src={images.originalIcon} />
-				<span>originals</span>
-			</MenuItem>
-		</Container>
-	)
+  return (
+    <Container>
+      <MenuItem>
+        <MenuItemImg src={images.homeIcon} />
+        <span>home</span>
+      </MenuItem>
+      <MenuItem>
+        <MenuItemImg src={images.searchIcon} />
+        <span>search</span>
+      </MenuItem>
+      <MenuItem>
+        <MenuItemImg src={images.watchIcon} />
+        <span>watchlist</span>
+      </MenuItem>
+      <MenuItem>
+        <MenuItemImg src={images.seriesIcon} />
+        <span>series</span>
+      </MenuItem>
+      <MenuItem>
+        <MenuItemImg src={images.movieIcon} />
+        <span>movies</span>
+      </MenuItem>
+      <MenuItem>
+        <MenuItemImg src={images.originalIcon} />
+        <span>originals</span>
+      </MenuItem>
+    </Container>
+  )
 }
 
 export default Nav
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import styled from 'styled-components'
-import { images } from '../../constants/images'
-import Logo from '../Logo'
-import Nav from './Nav'
-import UserImg from './UserImg'
+import React from "react"
+import styled from "styled-components"
+import { images } from "../../constants/images"
+import Logo from "../Logo"
+import Nav from "./Nav"
+import UserImg from "./UserImg"
 
 const Container = styled.div`
-	background-color: #090101;
-	height: 70px;
-	display: flex;
-	align-items: center;
+  background-color: #090101;
+  height: 70px;
+  display: flex;
+  align-items: center;
 `
 
 function Header() {
-	return (
-		<Container>
-			<Logo src={images.logo} />
-			<Nav />
-			<UserImg src={images.userImage} />
-		</Container>
-	)
+  return (
+    <Container>
+      <Logo src={images.logo} />
+      <Nav />
+      <UserImg src={images.userImage} />
+    </Container>
+  )
 }
 
 export default Header
